bdfda1e13983308f4b5a313b25296a3703d32eff
ljw2487 <ljw248769961@qq.com>
2022-05-18 23:07:47 +0800

    518

--- www/src/views/0.0_login.vue
+++ www/src/views/0.0_login.vue
@@ -1,10 +1,34 @@
 <template>
-    <div class="backPic" :style="{backgroundImage: `url(${backImgUrl})`}">
-        <div style="padding-top: 150px">
-            <div class="logo" style="background-color: red; width:120px; height:120px; margin: 0 auto; border-radius: 1000px">XX</div>
-            
+    <!-- <div class="backPic" :style="{backgroundImage: `url(${backImgUrl})`}"> -->
+        <div class="background">
+            <div class="logo">XX</div>
+            <div class="loginForm">
+                <van-form @submit="onSubmit">
+                    <van-field
+                        v-model="username"
+                        name="用户名"
+                        label=""
+                        left-icon="smile-o"
+                        placeholder="用户名/邮箱"
+                        :rules="[{ required: true, message: '请填写用户名或邮箱' }]"
+                    />
+                    <van-field
+                        v-model="password"
+                        type="password"
+                        name="密码"
+                        label=""
+                        left-icon="smile-o"
+                        placeholder="密码"
+                        :rules="[{ required: true, message: '请填写密码' }]"
+                    />
+                    <div style="margin: 16px;">
+                        <van-button round block type="info" native-type="submit">注册</van-button>
+                        <van-button round block type="info" native-type="submit">登录</van-button>
+                    </div>
+                </van-form>
+            </div>
         </div>
-    </div>
+    <!-- </div> -->
 </template>
 
 <script>
@@ -12,27 +36,28 @@ import { mapState, mapMutations } from "vuex"
     export default {
         data() {
             return {
-                backImgUrl: '',
+                username: '',
+                password: '',
             }
         },
         created () {
             // if (this.loginPageBackImgUrl == '') {
             //     this.getBackImgUrl()
             // }
-            this.axios.get(`${this.defaultLocalUrl}/setutime`, { 
-                params: {
-                    tag: "ロリ|萝莉|贫乳",
-                    size: "regular",
-                },
-            })
-            .then((res) => {
-                this.backImgUrl = res.data.data[0].urls.regular
-                console.log(res)
-                console.log(this.backImgUrl)
-            })
-            .catch((err) => {
-                console.error(err)
-            })
+            // this.axios.get(`${this.defaultLocalUrl}/setutime`, { 
+            //     params: {
+            //         tag: "ロリ|萝莉|贫乳",
+            //         size: "regular",
+            //     },
+            // })
+            // .then((res) => {
+            //     this.backImgUrl = res.data.data[0].urls.regular
+            //     console.log(res)
+            //     console.log(this.backImgUrl)
+            // })
+            // .catch((err) => {
+            //     console.error(err)
+            // })
         },
         mounted () {
             
@@ -41,6 +66,9 @@ import { mapState, mapMutations } from "vuex"
             ...mapMutations([
                 'getBackImgUrl', //also supports payload `this.nameOfMutation(amount)` 
             ]),
+            onSubmit(values) {
+                console.log('submit', values);
+            },
         },
         computed: {
             ...mapState([
@@ -53,23 +81,40 @@ import { mapState, mapMutations } from "vuex"
 </script>
 
 <style lang="scss" scoped>
-.backPic {
-    position: relative;
-    width: 100%;
-    height: 100%;
-    background-color: #2e2e2e;
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position: top center;
+// .backPic {
+//     position: relative;
+//     width: 100%;
+//     height: 100%;
+//     background-color: #2e2e2e;
+//     background-size: cover;
+//     background-repeat: no-repeat;
+//     background-position: top center;
     
-}
-.backPic::before {
-    content: '';
-    position: absolute;
-    left: 0;
-    top: 0;
-    width: 100%;
+// }
+// .backPic::before {
+//     content: '';
+//     position: absolute;
+//     left: 0;
+//     top: 0;
+//     width: 100%;
+//     height: 100%;
+//     background-color: rgba(36, 36, 36, 0.8);
+// }
+.background {
+    background-color: #2E2E2E;
+    padding-top: 11rem;
+    padding-left: 1rem;
+    padding-right: 1rem;
+    // width: 100%;
     height: 100%;
-    background-color: rgba(36, 36, 36, 0.8);
+}
+.loginForm {
+    background-color: #777777;
+    .van-form {
+        &div>:first-child {
+            background-color: #2E2E2E !important;
+            padding-bottom: 1rem !important;
+        }
+    }
 }
 </style>
